12058bf1cba1dec7d39bcbd86aa3bf5348e95aaa
Lungelo-M-Dlamini <neymardlamini@gmail.com>
2024-07-30 17:09:11 +0200

    Add project files.

---
+++ Day1_part1/Program.cs
@@ -0,0 +1,14 @@
+﻿long total = 0;
+string[] lines = File.ReadAllLines("C:\\Users\\Lungelo\\source\\repos\\ConsoleApp2\\ConsoleApp2\\Input.txt");
+
+foreach (var line in lines)
+{
+    int firstDigit = line.First(c => char.IsDigit(c)) - '0';
+    int lastDigit = line.Last(c => char.IsDigit(c)) - '0';
+
+    var fullNumber = firstDigit * 10 + lastDigit;
+    total += fullNumber;
+}
+
+Console.WriteLine(total);
+Console.ReadLine();
